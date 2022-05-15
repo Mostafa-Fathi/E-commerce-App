43c91f42bc009e22eb1b099433d231dc88ab01b7
Mostafa Fathy <mostafafathy677@gmail.com>
2022-05-15 05:27:12 +0200

    fix Color and text of add address page

--- E-commerce-App/E-commerce-App/E-commerce-App/Views/AddAddressess.xaml.cs
+++ E-commerce-App/E-commerce-App/E-commerce-App/Views/AddAddressess.xaml.cs
@@ -17,24 +17,5 @@ namespace E_commerce_App.Views
             InitializeComponent();
         }
 
-        private void cls1_defaultAddress(object sender, EventArgs e)
-        {
-            btn_Yes.TextColor =Color.White;
-            btn_Yes.BackgroundColor = Color.Green;
-            btn_No.TextColor = Color.Black;
-            btn_No.BackgroundColor = Color.White;
-            default_Address_label.Text = "This address is default shipping address";
-            default_Address_label.TextColor= Color.Green;
-        }
-
-        private void cls2_defaultAddress(object sender, EventArgs e)
-        {
-            btn_No.TextColor = Color.White;
-            btn_No.BackgroundColor = Color.Green;
-            btn_Yes.TextColor = Color.Black;
-            btn_Yes.BackgroundColor = Color.White;
-            default_Address_label.Text = "This is NOT default shipping address";
-            default_Address_label.TextColor = Color.Gray;
-        }
     }
 }
